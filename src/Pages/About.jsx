9829ee7014e7f27af275b9f204109eba9a49d9aa
Jitu gandhare <jitugandhare@gmail.com>
2024-05-16 12:20:20 +0530

    done

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -71,7 +71,7 @@ export const About = () => {
               boxShadow="dark-md"
               p="6"
               rounded="md"
-              src="https://camo.githubusercontent.com/c1dcb74cc1c1835b1d716f5051499a2814c683c806b15f04b0eba492863703e9/68747470733a2f2f63646e2e6472696262626c652e636f6d2f75736572732f3733303730332f73637265656e73686f74732f363538313234332f6176656e746f2e676966"
+              src="https://media.giphy.com/media/dWesBcTLavkZuG35MI/giphy.gif"
               borderTopRadius={"50%"}
               borderLeftRadius={"50%"}
             />
